52d5dd54ff4c75b8a2b8db0a355eb8a0a05a60fa
Sindhura17 <sindhura.l.bhat@gmail.com>
2020-03-06 21:16:48 +0530

    new activity

--- payment_app/app/src/main/java/com/example/payment_app/MainActivity.java
+++ payment_app/app/src/main/java/com/example/payment_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.payment_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void next(View view){
+        Intent intent = new Intent(this, test.class);
+        startActivity(intent);
     }
 }
