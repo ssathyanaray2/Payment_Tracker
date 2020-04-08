7f998fe262c4234ecc552e1d97e32521581770dc
Sindhura17 <sindhura.l.bhat@gmail.com>
2020-04-08 11:57:12 +0530

    input

--- payment_app/app/src/main/java/com/example/payment_app/popup.java
+++ payment_app/app/src/main/java/com/example/payment_app/popup.java
@@ -3,16 +3,27 @@ package com.example.payment_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class popup extends AppCompatActivity {
 
+    private EditText namev;
+    private EditText datev;
+    private EditText descv;
+    private EditText intervalv;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_popup);
         DisplayMetrics dm=new DisplayMetrics();
@@ -20,6 +31,34 @@ public class popup extends AppCompatActivity {
         int width=dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int)(width*0.7),(int)(height*0.7));
+        namev= findViewById(R.id.name);
+        descv = findViewById(R.id.desc);
+        datev = findViewById(R.id.date);
+        intervalv = findViewById(R.id.interval);
+        Button add=findViewById(R.id.add);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_reminder();
+            }
+            });
+
+
+
+    }
+
+    private void add_reminder(){
+        String name= namev.getText().toString();
+        String desc= descv.getText().toString();
+        String date= datev.getText().toString();
+        String temp=intervalv.getText().toString();
+        int interval=Integer.parseInt(temp);
+        interval+=2;
+        String u=String.valueOf(interval);
+        String t=name+desc+date+u;
+        TextView test=findViewById(R.id.test);
+        test.setText(t);
     }
 
 
