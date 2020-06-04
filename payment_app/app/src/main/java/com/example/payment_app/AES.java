1946a091302d261e4cc9c75ea0b59e58eb1752fb
iamspoo <spoorthi246@gmail.com>
2020-06-04 19:09:17 +0530

    update aes

--- payment_app/app/src/main/java/com/example/payment_app/AES.java
+++ payment_app/app/src/main/java/com/example/payment_app/AES.java
@@ -4,7 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.Base64;
+import android.util.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -39,7 +39,8 @@ public class AES {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+            //return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+            return Base64.encodeToString(strToEncrypt.getBytes("UTF-8"), Base64.DEFAULT);
         }
         catch (Exception e)
         {
@@ -55,7 +56,8 @@ public class AES {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+            //return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+            return new String(Base64.decode(strToDecrypt,Base64.DEFAULT));
         }
         catch (Exception e)
         {
